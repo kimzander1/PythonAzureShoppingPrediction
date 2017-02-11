f6ca41a7184e7cf9283003b359822c61c287841b
Kabba Abbaa <KDA9695@GMAIL.COM>
2017-02-11 07:38:05 -0500

    Whoo Hoo Working!  Had to create ironpyton_config.py and add django_extensions to settings.py INSTALLED_APPS, then run python manage.py shell_plus --notebook and WALLAH! I now have the enviable privilige of using jupyter with django support as addressed in article https://www.calazan.com/using-ipython-notebook-django-shell-inside-docker-container/

--- PyCon2015AzureTutorial/settings.py
+++ PyCon2015AzureTutorial/settings.py
@@ -141,6 +141,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    'django_extensions',
 )
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
